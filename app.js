a11879aa240f1bdcb5cc80213d4295a65fc1a41f
David MacCallum <davidjohnmac@gmail.com>
2021-04-08 18:39:11 +0200

    Index

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.render('index', {})
 })
 
+app.get('/sermon-publisher', (req, res) => {
+  res.render('sermon-publisher', {})
+})
+
 app.get('/design-language', (req, res) => {
   res.render('design-language', {})
 })
